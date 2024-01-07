64b8ec0c05361f35766227c3a5f0a521e4160a7d
aman <avinashthakurchauhan07@gmail.com>
2024-01-07 12:51:34 +0530

    adding the static popup pdp in collection

--- src/Pages/ProductCollection.jsx
+++ src/Pages/ProductCollection.jsx
@@ -5,9 +5,11 @@ import ProductGrid from '../Components/ProductCollectionComp/ProductGrid';
 import ProductCollectionVideoComp from '../Components/ProductCollectionComp/ProductCollectionVideoComp';
 import ProductCollectionAlsoLike from '../Components/ProductCollectionComp/ProductCollectionAlsoLike';
 import FilterAndShortDesktop from '../Components/ProductCollectionComp/filteAndShortDesktop';
+import PdpPopup from '../common/pdpPopUp';
 
 const ProductCollection = () => {
     return <>
+    <PdpPopup/>
     <section className='w-full relative h-fit '>
         <div className='inner-container bg-white w-full h-fit px-[25px]'>
             <div className='content-container w-full h-fit pt-[37px] md:pt-[8rem] lg:pt-[6.5rem]'>
@@ -25,10 +27,10 @@ const ProductCollection = () => {
             
         </div>
         <div>
-            <div>
+            <div className='lg:mt-[2rem] '>
                 <ProductCollectionVideoComp />
             </div>
-            <div>
+            <div className=''>
                 <ProductCollectionAlsoLike />
             </div>
         </div>
